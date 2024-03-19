f96438ae8d5e58e969b5672275945c39f8021156
vinaylpinfotech <pooja.chauhan@lpinfotech.com>
2024-03-19 11:21:03 +0530

    statup_code

---
+++ API/my.blogs.DbModels/AppUsers.cs
@@ -0,0 +1,11 @@
+﻿namespace my.blogs.DbModels
+{
+    public class AppUsers
+    {
+        public int PkId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+          
+    }
+}
